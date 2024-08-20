53d2da693fa403d5b3938fad7c2d8fafd9a0b4e7
Kechter <Kechter@users.noreply.github.com>
2024-08-20 18:44:45 +0000

    deploy: 401021101a46c5b5ccd0868d6299b17f76ee2162

---
+++ 67.5576fdb733f62b64.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkFamily_App=self.webpackChunkFamily_App||[]).push([[67],{67:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
